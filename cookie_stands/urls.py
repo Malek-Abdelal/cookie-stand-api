81b733f8ea570e49f6bbb067efe04bf78ec3ffa2
Malek-Abdelal <malekbassam1144@gmail.com>
2023-08-17 23:45:14 +0300

    Done

--- cookie_stands/urls.py
+++ cookie_stands/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import CookieStandList, CookieStandDetail
 
 urlpatterns = [
-    path("", CookieStandList.as_view(), name="cookie_stand_list"),
+    path("", CookieStandList.as_view(), name="cookie_stand_list1"),
     path("<int:pk>/", CookieStandDetail.as_view(), name="cookie_stand_detail"),
 ]
